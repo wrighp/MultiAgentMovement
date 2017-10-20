3711cac6502662aeeae2d76e69da4fccf7d37523
samsuite <suitester@gmail.com>
2017-10-19 21:17:40 -0400

    better boids

--- Assets/Code/FollowerEmergent.cs
+++ Assets/Code/FollowerEmergent.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class FollowerEmergent : MonoBehaviour {
 
+
+    public Path my_path;
+    public static int path_index = 0;
+    float arrival_radius = 1f;
+
+
     // 3 things:
     // - steer in direction of nearby boids
     // - avoid colliding with nearby boids 
     // - move towards average position of nearby boids
 
-    const float separation_amount = 0f;
+    const float separation_amount = 4f;
     const float alignment_amount = 1f;
-    const float cohesion_amount = 1f;
+    const float cohesion_amount = 3f;
 
-    const float local_range = 3f;
+    const float local_range = 1f;
 
     Vector3 separation_direction = new Vector3(0f,0f,0f);
     Vector3 alignment_direction = new Vector3(0f,0f,0f);
@@ -29,7 +35,6 @@ public class FollowerEmergent : MonoBehaviour {
     void Update () {
         List<Boid> local_boids = new List<Boid>();
         Boid[] all_boids = Object.FindObjectsOfType<Boid>();
-        Boid my_leader = Object.FindObjectOfType<LeaderEmergent>().gameObject.GetComponent<Boid>();
 
         // make a list of nearby boids
         for (int i = 0; i < all_boids.Length; i++) {
@@ -51,7 +56,7 @@ public class FollowerEmergent : MonoBehaviour {
         separation_direction = separation_direction.normalized;
 
 
-
+        /*
         alignment_direction = new Vector3(0f,0f,0f);
 
         // sum the directions of the local boids
@@ -62,6 +67,22 @@ public class FollowerEmergent : MonoBehaviour {
 
         alignment_direction += (Vector3)my_leader.rb.velocity * 10f;
         alignment_direction = alignment_direction.normalized;
+        */
+
+
+
+        if (my_path == null) {
+			return;
+		}
+        alignment_direction = (my_path.points[path_index].position - transform.position).normalized;
+
+        if (Vector3.Distance(transform.position, my_path.points[path_index].position) < arrival_radius) {
+            path_index = (path_index + 1)%my_path.points.Count;
+        }
+
+
+
+
 
 
         cohesion_direction = new Vector3(0f,0f,0f);
